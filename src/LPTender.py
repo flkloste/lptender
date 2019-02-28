35622d495f8e76186989a7e5908ddd0d41c66791
flkloste <flkloste@uos.de>
2019-02-28 20:31:20 +0100

    Added servo types

--- src/LPTender.py
+++ src/LPTender.py
@@ -6,18 +6,16 @@ from time import sleep
 
 if __name__ == "__main__":
     gpio = GlobalGPIO.GlobalGPIO()
-    elevator = Elevator.Elevator(gpio)
-    servo = Servo.Servo(gpio, 17)
-    servo.setAngle(70)
-    sleep(0.5)
-    servo.dontMove()
-    sleep(10)
-    servo.setAngle(86)
-    sleep(0.5)
-    servo.dontMove()
-
-    elevator.gotoHome()
-    elevator.moveDown(20000)
+    #elevator = Elevator.Elevator(gpio)
     
-    elevator.moveUp(10000)
+    servo = Servo.ServoDS3218_180(gpio, 17)
+    
+    for i in range (1,100):
+        servo.setAngle(2 + float(i)/100)
+        sleep(0.5)
+    
+    #elevator.gotoHome()
+    #elevator.moveDown(20000)
+    
+    #elevator.moveUp(10000)
     
--- src/Servo.py
+++ src/Servo.py
@@ -11,12 +11,30 @@ class Servo:
         self._pwm.start(0)
         
     def setAngle(self, angle):
+        raise RuntimeError("Not implemented!")
+        
+    def dontMove(self):
+        self._pwm.ChangeDutyCycle(0)
+
+class ServoDS3218_270(Servo):
+    def __init__(self, globalGPIO, gpioControl):
+        super(ServoDS3218_270, self).__init__(globalGPIO, gpioControl)
+    
+    def setAngle(self, angle):
         if not (0 <= angle <= 270): 
             raise RuntimeError("Angle out of range. (angle=%s)" % str(angle))
  
         dutyCycle = 2.22222222 + 10.0 / 270.0 * angle
-        print dutyCycle
         self._pwm.ChangeDutyCycle(dutyCycle)
         
-    def dontMove(self):
-        self._pwm.ChangeDutyCycle(0)
+class ServoDS3218_180(Servo):
+    def __init__(self, globalGPIO, gpioControl):
+        super(ServoDS3218_180, self).__init__(globalGPIO, gpioControl)
+    
+    def setAngle(self, angle):
+       # if not (0 <= angle <= 180): 
+        #    raise RuntimeError("Angle out of range. (angle=%s)" % str(angle))
+ 
+       # dutyCycle = 2.22222222 + 10.0 / 270.0 * angle
+        print dutyCycle
+        self._pwm.ChangeDutyCycle(dutyCycle)
