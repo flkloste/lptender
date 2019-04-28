151aa57ccf9cbbfebd16d7431f14097cee9835de
flkloste <flkloste@uos.de>
2019-04-28 20:12:54 +0200

    gripper adjuments

--- src/Gripper.py
+++ src/Gripper.py
@@ -3,25 +3,27 @@ from time import sleep
 
 class Gripper:
     def __init__(self, globalGPIO):
-        self._gpioLeftServoControl = XX
-        self._gpioRightServoControl = XX
-        self._leftServo = Servo.ServoDS3218_180(globalGPIO, _gpioLeftServoControl)
-        self._rightServo = Servo.ServoDS3218_180(globalGPIO, _gpioRightServoControl)
+        self._gpioLeftServoControl = 27
+        self._gpioRightServoControl = 22
+        self._leftServo = Servo.ServoDS3218_180(globalGPIO, self._gpioLeftServoControl)
+        self._rightServo = Servo.ServoDS3218_180(globalGPIO, self._gpioRightServoControl)
         self._rightServoCloseAngle = 100       
         self._rightServoOpenAngle = 80
         self._leftServoCloseAngle = 80
         self._leftServoOpenAngle = 100
         
     def grip(self):
-        self._leftServo.setAngle(_leftServoCloseAngle)
-        self._rightServo.setAngle(_rightServoCloseAngle)
+        self._leftServo.setAngle(self._leftServoCloseAngle)
+        self._rightServo.setAngle(self._rightServoCloseAngle)
         sleep(1)
-        servo.dontMove()
+        self._leftServo.dontMove()
+        self._rightServo.dontMove()
         
     def release(self):
-        self._leftServo.setAngle(_leftServoOpenAngle)
-        self._rightServo.setAngle(_rightServoOpenAngle)
+        self._leftServo.setAngle(self._leftServoOpenAngle)
+        self._rightServo.setAngle(self._rightServoOpenAngle)
         sleep(1)
-        servo.dontMove()
+        self._leftServo.dontMove()
+        self._rightServo.dontMove()
         
     
