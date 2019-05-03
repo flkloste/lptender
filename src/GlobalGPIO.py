8fc4be4e99c8d9c3e1338f635e875826b16c9930
flkloste <flkloste@uos.de>
2019-05-03 20:05:36 +0200

    use pigpio instead of RPi.GPIO

--- src/GlobalGPIO.py
+++ src/GlobalGPIO.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+import pigpio
 from time import sleep
 
 class GlobalGPIO:
@@ -7,19 +7,22 @@ class GlobalGPIO:
         self.usedOutputGPIOs = list()
         self.usedInputGPIOs = list()
         self.usedPwms = list()
-        GPIO.setmode(GPIO.BCM)
+        self._pi = pigpio.pi()
         
     def __del__(self):
+        # stop all pwm output (servo)
         for p in self.usedPwms:
-            p.stop
-        GPIO.cleanup()
+            #p.stop
+            self._pi.set_servo_pulsewidth(pi, 0)  
+        self._pi.stop()
         sleep(2)
 
     def setupOutput(self, gpioBcmNo):
         if gpioBcmNo in self.usedOutputGPIOs or gpioBcmNo in self.usedInputGPIOs:
             raise RuntimeError("GPIO %s already in use!" % str(gpioBcmNo))
             
-        GPIO.setup(gpioBcmNo, GPIO.OUT)
+        #GPIO.setup(gpioBcmNo, GPIO.OUT)
+        self._pi.set_mode(gpioBcmNo, pigpio.OUTPUT)
         self.usedOutputGPIOs.append(gpioBcmNo)
     
     def setupInput(self, gpioBcmNo, pullUpDown=None):
@@ -30,9 +33,12 @@ class GlobalGPIO:
             raise RuntimeError("Invalid value for pull up/down: %s" % str(pullUpDown))
             
         if pullUpDown is None:
-            GPIO.setup(gpioBcmNo, GPIO.IN)
+            #GPIO.setup(gpioBcmNo, GPIO.IN)
+            self._pi.set_mode(gpioBcmNo, pigpio.INPUT)
         else:
-            GPIO.setup(gpioBcmNo, GPIO.IN, pull_up_down=pullUpDown)
+            #GPIO.setup(gpioBcmNo, GPIO.IN, pull_up_down=pullUpDown)
+            self._pi.set_mode(gpioBcmNo, pigpio.INPUT)
+            self._pi.set_pull_up_down(gpioBcmNo, pullUpDown)
         self.usedInputGPIOs.append(gpioBcmNo)
     
     def output(self, gpioBcmNo, level):
@@ -42,30 +48,48 @@ class GlobalGPIO:
         if gpioBcmNo not in self.usedOutputGPIOs:
             raise RuntimeError("GPIO %s has not been set up as an output!" % str(gpioBcmNo))
             
-        GPIO.output(gpioBcmNo, level) 
+        #GPIO.output(gpioBcmNo, level) 
+        self._pi.write(gpioBcmNo, level)
     
     def input(self, gpioBcmNo):
         if gpioBcmNo not in self.usedInputGPIOs:
             raise RuntimeError("GPIO %s has not been set up as an input!" % str(gpioBcmNo))
             
-        return GPIO.input(gpioBcmNo)
+        #return GPIO.input(gpioBcmNo)
+        return self._pi.read(gpioBcmNo)
         
-    def pwm(self, gpioBcmNo, frequency):
-        if gpioBcmNo not in self.usedOutputGPIOs:
-            raise RuntimeError("GPIO %s has not been set up as an output!" % str(gpioBcmNo))
+#    def pwm(self, gpioBcmNo, frequency):
+#        if gpioBcmNo not in self.usedOutputGPIOs:
+#            raise RuntimeError("GPIO %s has not been set up as an #output!" % str(gpioBcmNo))
+#            
+#        p = GPIO.PWM(gpioBcmNo, frequency)
+#        self.usedPwms.append(p)
+#        return p
+
+    def setServoPulseWidth(self, gpioBcmNo, pulseWidth):
+        if gpioBcmNo in self.usedOutputGPIOs or gpioBcmNo in self.usedInputGPIOs:
+            raise RuntimeError("GPIO %s already in use!" % str(gpioBcmNo))
             
-        p = GPIO.PWM(gpioBcmNo, frequency)
-        self.usedPwms.append(p)
-        return p
+        if pulseWidth not in range(500, 2500) and pulseWidth != 0:
+            raise RuntimeError("Invalid value for pulseWidth: %s" % str(pullUpDown))
+
+        self._pi.set_servo_pulsewidth(gpioBcmNo, pulseWidth)
+        
+        if gpioBcmNo not in self.usedPwms:
+            self.usedPwms.append(gpioBcmNo)
 
     def levelHigh(self):
-        return GPIO.HIGH
+        #return GPIO.HIGH
+        return 1
 
     def levelLow(self):
-        return GPIO.LOW
+        #return GPIO.LOW
+        return 0
 
     def inputPullDown(self):
-        return GPIO.PUD_DOWN
+        #return GPIO.PUD_DOWN
+        return pigpio.PUD_DOWN
         
     def inputPullUp(self):
-        return GPIO.PUD_UP
+        #return GPIO.PUD_UP
+        return pigpio.PUD_UP
--- src/Servo.py
+++ src/Servo.py
@@ -2,18 +2,20 @@ class Servo(object):
     def __init__(self, globalGPIO, gpioControl):
         self._gpio = globalGPIO
         self._gpioControl = gpioControl
-        self._frequency = 50
+        #self._frequency = 50
         
         # setup control GPIO for PWM
-        self._gpio.setupOutput(self._gpioControl)
-        self._pwm = self._gpio.pwm(self._gpioControl, self._frequency)
-        self._pwm.start(0)
+        #self._gpio.setupOutput(self._gpioControl)
+        #self._pwm = self._gpio.pwm(self._gpioControl, self._frequency)
+        #self._pwm.start(0)
+        
         
     def setAngle(self, angle):
         raise RuntimeError("Not implemented!")
         
     def dontMove(self):
-        self._pwm.ChangeDutyCycle(0)
+        #self._pwm.ChangeDutyCycle(0)
+        self._gpio.setServoPulseWidth(self._gpioControl, 0)
 
 class ServoDS3218_270(Servo):
     def __init__(self, globalGPIO, gpioControl):
@@ -23,8 +25,10 @@ class ServoDS3218_270(Servo):
         if not (0 <= angle <= 270): 
             raise RuntimeError("Angle out of range. (angle=%s)" % str(angle))
  
-        dutyCycle = 2.22222222 + 10.0 / 270.0 * angle
-        self._pwm.ChangeDutyCycle(dutyCycle)
+        #dutyCycle = 2.22222222 + 10.0 / 270.0 * angle
+        #self._pwm.ChangeDutyCycle(dutyCycle)
+        pulseWidth = 500 + 2000.0 / 270.0 * angle
+        self._gpio.setServoPulseWidth(self._gpioControl, pulseWidth)
         
 class ServoDS3218_180(Servo):
     def __init__(self, globalGPIO, gpioControl):
@@ -34,5 +38,7 @@ class ServoDS3218_180(Servo):
         if not (0 <= angle <= 180): 
             raise RuntimeError("Angle out of range. (angle=%s)" % str(angle))
  
-        dutyCycle = 2 + 10.0 / 180.0 * angle
-        self._pwm.ChangeDutyCycle(dutyCycle)
+        #dutyCycle = 2 + 10.0 / 180.0 * angle
+        #self._pwm.ChangeDutyCycle(dutyCycle)
+        pulseWidth = 500 + 2000.0 / 180.0 * angle
+        self._gpio.setServoPulseWidth(self._gpioControl, pulseWidth)
