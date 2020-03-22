0992120d5d1fe2c8165d171fe160b0b5c8c0386b
flkloste <flkloste@uos.de>
2020-03-22 14:32:41 +0000

    Gripper: grip slowly in half angle steps

--- src/Gripper.py
+++ src/Gripper.py
@@ -9,12 +9,31 @@ class Gripper:
         self._rightServo = Servo.ServoDS3218_180(globalGPIO, self._gpioRightServoControl)
         self._rightServoCloseAngle = 106
         self._rightServoOpenAngle = 99
+        self._rightServoCurrentAngle = -1
         self._leftServoCloseAngle = 73 
         self._leftServoOpenAngle = 79
+        self._leftServoCurrentAngle = -1
+
         
     def grip(self):
-        self._leftServo.setAngle(self._leftServoCloseAngle)
-        self._rightServo.setAngle(self._rightServoCloseAngle)
+        if ( self._leftServoCurrentAngle < 0 
+             or self._rightServoCurrentAngle < 0
+             or self._leftServoCurrentAngle < self._leftServoCloseAngle
+             or self._rightServoCurrentAngle > self._rightServoCloseAngle ):
+            self._leftServo.setAngle(self._leftServoCloseAngle)
+            self._rightServo.setAngle(self._rightServoCloseAngle)
+            self._leftServoCurrentAngle = self._leftServoCloseAngle
+            self._rightServoCurrentAngle = self._rightServoCloseAngle
+        else:
+            for i in range(max((self._rightServoCloseAngle - self._rightServoCurrentAngle), (self._leftServoCurrentAngle - self._leftServoCloseAngle))*2):
+                if(self._leftServoCurrentAngle > self._leftServoCloseAngle):
+                    self._leftServoCurrentAngle -= 0.5
+                    self._leftServo.setAngle(self._leftServoCurrentAngle)
+                if(self._rightServoCurrentAngle < self._rightServoCloseAngle):
+                    self._rightServoCurrentAngle += 0.5
+                    self._rightServo.setAngle(self._rightServoCurrentAngle)
+                sleep(0.1)
+
         sleep(1)
         self._leftServo.dontMove()
         self._rightServo.dontMove()
@@ -25,5 +44,7 @@ class Gripper:
         sleep(1)
         self._leftServo.dontMove()
         self._rightServo.dontMove()
+        self._leftServoCurrentAngle = self._leftServoOpenAngle
+        self._rightServoCurrentAngle = self._rightServoOpenAngle
         
     
